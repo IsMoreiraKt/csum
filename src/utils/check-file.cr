e6e764549487e32cc5197f0d7f41c7985e8a72ea
IsMoreiraKt <ismaelmoreiraubuntu@gmail.com>
2024-11-12 13:17:42 +0000

    feat: Addition of methods to validate user input.

---
+++ src/utils/check-file.cr
@@ -0,0 +1,42 @@
+#################################################
+#                                               #
+#            csum - Checkfile Module            #
+#                                               #
+#                                               #
+# This module is part of the csum project,      #
+# a checksum utility based on coreutils `sum`.  #
+# It was created for educational purposes to    #
+# show how checksums work and to learn about    #
+# system programming.                           #
+#                                               #
+# The csum project is licensed under the GPL    #
+# version 3.0 or later.                         #
+#                                               #
+# You can find the source code at:              #
+# https://github.com/IsMoreiraKt/csum           #
+#                                               #
+# This module is inspired by the original BSD   #
+# `sum` utility, which can be found here:       #
+# https://github.com/coreutils/coreutils        #
+#                                               #
+#################################################
+
+require "posix/stat"
+
+module CheckFile
+  def is_valid_path?(path : String) : Int32
+    if File.exists?(path)
+      return 0
+    else
+      return -1
+    end
+  end
+
+  def is_file?(path : String) : Int32
+    if File.exists?(path) && File.info(path).file?
+      return 0
+    else
+      return -1
+    end
+  end
+end
